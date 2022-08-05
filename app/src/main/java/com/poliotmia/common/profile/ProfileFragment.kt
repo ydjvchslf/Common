64f56dae3cace57192faf138cbb0fb0ef83094ac
poliot-mia <mikyung.yong@poliot.co.kr>
2022-08-05 15:49:44 +0900

    glide 작어중

--- app/src/main/java/com/poliotmia/common/profile/ProfileFragment.kt
+++ app/src/main/java/com/poliotmia/common/profile/ProfileFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FirebaseFirestore
 import com.poliotmia.common.databinding.FragmentProfileBinding
 import com.poliotmia.common.util.DebugLog
 
@@ -12,6 +14,7 @@ class ProfileFragment : Fragment() {
 
     private val logTag = ProfileFragment::class.simpleName
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var firestore: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +24,20 @@ class ProfileFragment : Fragment() {
         //return super.onCreateView(inflater, container, savedInstanceState)
         DebugLog.i(logTag, "onCreateView-()")
         binding = FragmentProfileBinding.inflate(inflater, container, false)
+        firestore = FirebaseFirestore.getInstance()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        DebugLog.i(logTag, "onViewCreated-()")
+        firestore.collection("images").get().addOnCompleteListener { task ->
+            if(task.isSuccessful) {
+                var photo = task.result?.toObject(Photo::class.java)
+                Glide.with(this).load(photo?.imageUrl).into(imageIv)
+                descriptionTv.text=photo?.description
+            }
+        }
+
     }
 }

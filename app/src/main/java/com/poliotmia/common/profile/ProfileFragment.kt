c4800b4786947c681a287c8e6b494a8eb829b53b
poliot-mia <mikyung.yong@poliot.co.kr>
2022-08-08 17:25:35 +0900

    database 에서 data 읽어오기 하는중

--- app/src/main/java/com/poliotmia/common/profile/ProfileFragment.kt
+++ app/src/main/java/com/poliotmia/common/profile/ProfileFragment.kt
@@ -1,12 +1,20 @@
 package com.poliotmia.common.profile
 
 import android.os.Bundle
+import android.os.Debug
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.poliotmia.common.Photo
 import com.poliotmia.common.databinding.FragmentProfileBinding
 import com.poliotmia.common.util.DebugLog
 
@@ -14,6 +22,8 @@ class ProfileFragment : Fragment() {
 
     private val logTag = ProfileFragment::class.simpleName
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var myRef: DatabaseReference
     private lateinit var firestore: FirebaseFirestore
 
     override fun onCreateView(
@@ -24,6 +34,8 @@ class ProfileFragment : Fragment() {
         //return super.onCreateView(inflater, container, savedInstanceState)
         DebugLog.i(logTag, "onCreateView-()")
         binding = FragmentProfileBinding.inflate(inflater, container, false)
+        auth = Firebase.auth
+        myRef = Firebase.database.getReference("users")
         firestore = FirebaseFirestore.getInstance()
         return binding.root
     }
@@ -31,13 +43,29 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         DebugLog.i(logTag, "onViewCreated-()")
-        firestore.collection("images").get().addOnCompleteListener { task ->
-            if(task.isSuccessful) {
-                var photo = task.result?.toObject(Photo::class.java)
-                Glide.with(this).load(photo?.imageUrl).into(imageIv)
-                descriptionTv.text=photo?.description
-            }
-        }
+        auth.currentUser?.uid.let { uid ->
+            DebugLog.d(logTag, "uid: $uid")
+            // Read from the database
+            myRef.addValueEventListener(object: ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val user = snapshot.child("$uid")
+                    for(ds in user.children){
+                        DebugLog.d(logTag, "======> ds.toString()")
+                    }
+                }
 
+                override fun onCancelled(error: DatabaseError) {
+                    DebugLog.d(logTag, "읽어오기 실패")
+                }
+            })
+
+            // [END read_message]
+
+//            firestore.collection("images").get().addOnCompleteListener { task ->
+//                if(task.isSuccessful) {
+//
+//                }
+//            }
+        }
     }
 }
